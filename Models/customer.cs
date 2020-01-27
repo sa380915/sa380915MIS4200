92b640408e89ddc6a03e396344441dd31e11d279
Sara Althauser <sa380915@ohio.edu>
2020-01-27 11:42:07 -0500

    Changes made on 1/27/2020, start modeling

---
+++ Models/customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sa380915MIS4200.Models
+{
+    public class customer
+    {
+        public int customerID { get; set; }
+        public string customerFirstName { get; set; }
+        public string customerLastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+
+    }
+}
