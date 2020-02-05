070482b5693c940cb93d0a4216a1533ff15c815a
Sara Althauser <sa380915@ohio.edu>
2020-02-05 11:44:24 -0500

    2/5/2020

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,8 @@ namespace sa380915MIS4200.DAL
         {
             // this method is a 'constructor' and is called when a new context is created
             // the base attribute says which connection string to use
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, sa380915MIS4200.Migrations.MISContext.Configuration>("DefaultConnection"));
+            
         }
 
         public DbSet<Orders> Orders { get; set; }
@@ -20,6 +23,12 @@ namespace sa380915MIS4200.DAL
         public DbSet<Product> Products { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
 
-        //public System.Data.Entity.DbSet<sa380915MIS4200.Models.Product> Product { get; set; }
+        // add this method - it will be used later
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
